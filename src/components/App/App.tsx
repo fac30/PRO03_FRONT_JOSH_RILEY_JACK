151dfb4d5efbcf640c7f1e210ffa358bfaadafbf
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-01 09:29:55 +0100

    setup tailwind and initalise state in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import GameMap from "../GameMap/GameMap";
 import "./App.css";
 
 const App = () => {
+  const [userScore, setUserScore] = useState<number>(0);
+  const [currentCountry, setCurrentCountry] = useState<string>("Zimbabwe");
+  const [userChoice, setUserChoice] = useState<string>("");
+
   return (
     <div>
       <GameMap />
