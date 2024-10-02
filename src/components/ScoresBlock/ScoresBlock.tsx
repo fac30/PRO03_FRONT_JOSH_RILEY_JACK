948330d195bc2f6e6cad42b0e4ca074822b1debd
rilez366 <rilez366@outlook.com>
2024-10-02 17:20:23 +0100

    comments-on-code

--- src/components/ScoresBlock/ScoresBlock.tsx
+++ src/components/ScoresBlock/ScoresBlock.tsx
@@ -2,7 +2,7 @@ import ScoreDisplay from "./ScoresDisplay/ScoreDisplay";
 
 const ScoresBlock = ({ userScore, highScore }) => {
   return (
-    <div>
+    <div className="ml-9 w-9">
       <ScoreDisplay displayType="highScore" score={highScore} />
       <ScoreDisplay score={userScore} />
     </div>
--- src/components/ScoresBlock/ScoresDisplay/ScoreDisplay.tsx
+++ src/components/ScoresBlock/ScoresDisplay/ScoreDisplay.tsx
@@ -1,6 +1,6 @@
 const ScoreDisplay = ({ displayType, score }) => {
   return (
-    <h1 className="mb-8 ml-28">
+    <h1 className=" ml-auto">,y
       I am a {displayType === "highScore" ? "high" : "user"} score display. The
       score is <span className="text-2xl">{score}</span>
     </h1>
