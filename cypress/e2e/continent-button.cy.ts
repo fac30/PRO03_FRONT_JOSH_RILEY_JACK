b00d9d3df0b11daf6f5c056e296ae6d5fc9a7dac
JoshCodedit <joshua.dixoncms@gmail.com>
2024-10-09 17:44:06 +0100

    Country fact test implemented

--- cypress/e2e/continent-button.cy.ts
+++ cypress/e2e/continent-button.cy.ts
@@ -12,14 +12,24 @@ describe("Continent Buttons", () => {
   ];
 
   continents.forEach((continent) => {
-    it(`generates a new country when '${continent}' clicked`, () => {
+    it(`generates a new country and fact when '${continent}' clicked`, () => {
       cy.get('[data-test="current-country"]').then(($currentCountry) => {
         const initialCountry = $currentCountry.text().trim();
 
-        cy.get(`[data-test="continent-button-${continent}"]`).click();
-        cy.get('[data-test="current-country"]').should(($newCountry) => {
-          const updatedCountry = $newCountry.text().trim();
-          expect(updatedCountry).to.not.equal(initialCountry);
+        cy.get('[data-test="country-fact"]').then(($currentFact) => {
+          const initialFact = $currentFact.text().trim();
+
+          cy.get(`[data-test="continent-button-${continent}"]`).click();
+
+          cy.get('[data-test="current-country"]').should(($newCountry) => {
+            const updatedCountry = $newCountry.text().trim();
+            expect(updatedCountry).to.not.equal(initialCountry);
+          });
+
+          cy.get('[data-test="country-fact"]').should(($newFact) => {
+            const updatedFact = $newFact.text().trim();
+            expect(updatedFact).to.not.equal(initialFact);
+          });
         });
       });
     });
