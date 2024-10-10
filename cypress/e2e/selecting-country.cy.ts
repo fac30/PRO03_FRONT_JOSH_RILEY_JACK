7ba75db3becf1f1d1407b1ed58dd1d9918e114d7
JoshCodedit <joshua.dixoncms@gmail.com>
2024-10-10 12:37:10 +0100

    test: create incorrect answer test

--- cypress/e2e/selecting-country.cy.ts
+++ cypress/e2e/selecting-country.cy.ts
@@ -20,4 +20,27 @@ describe("Countries", () => {
       });
     });
   });
+
+  it(`Tests that user loses guess when answered incorrectly.`, () => {
+    cy.wait(2000);
+
+    cy.get('[data-test="remaining-guesses"]').then((currentGuesses) => {
+      const initialGuesses = currentGuesses.text().trim();
+
+      cy.get('[data-test="current-country"]').then(($currentCountry) => {
+        const countryText = $currentCountry.text().trim();
+
+        if (countryText === "Spain") {
+          cy.get('[data-test="Italy"]').click();
+        } else {
+          cy.get('[data-test="Spain"]').click();
+        }
+      });
+
+      cy.get('[data-test="remaining-guesses"]').should((newGuesses) => {
+        const updatedGuesses = newGuesses.text().trim();
+        expect(updatedGuesses).to.not.equal(initialGuesses);
+      });
+    });
+  });
 });
